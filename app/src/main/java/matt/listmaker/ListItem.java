ba781a5b8fd07deff204be1de35c925bc2a8d4ea
Matthew <matthewhunter404@gmail.com>
2017-01-04 12:04:14 +0300

    Creates the ListItem Class. This is copied entirely from my old Lister project.

---
+++ app/src/main/java/matt/listmaker/ListItem.java
@@ -0,0 +1,31 @@
+package matt.listmaker;
+
+/**
+ * Created by MattsDesktop on 04/01/2017.
+ */
+public class ListItem {
+    private String itemText; //The items text
+    private int keyID; //this links this list item to the unique ID of a list
+
+    public ListItem(String pText, int pkeyID) {
+        itemText=pText;
+        keyID=pkeyID;
+    }
+
+    public void setItemText(String pText) {
+        itemText=pText;
+    }
+
+    public String getItemText(){
+        return itemText;
+    }
+
+    public void setItemKeyID(int pkeyID) {
+        keyID=pkeyID;
+    }
+
+    public int getItemKeyID(){
+        return keyID;
+    }
+
+}
