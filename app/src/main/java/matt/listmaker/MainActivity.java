b14e896fbec78085bda438e15529f6b0aa40bc5b
Matthew <matthewhunter404@gmail.com>
2017-01-09 22:27:02 +0300

    adds dbHelper to Mainactivity

--- app/src/main/java/matt/listmaker/MainActivity.java
+++ app/src/main/java/matt/listmaker/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListDBHelper dbHelper;
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
