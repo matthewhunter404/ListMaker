8cb76fc687acebf441606cf962f958f183a3cf0a
Matthew <matthewhunter404@gmail.com>
2017-02-05 16:09:38 +0300

    Adds a Listview to ListActivity, both in the xml file and the class

--- app/src/main/java/matt/listmaker/ListActivity.java
+++ app/src/main/java/matt/listmaker/ListActivity.java
@@ -2,16 +2,19 @@ package matt.listmaker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 /**
  * Created by MattsDesktop on 02/02/2017.
  */
 
 public class ListActivity extends AppCompatActivity {
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        mListView = (ListView) findViewById(R.id.ListObjectDisplayList);
     }
 
 }
