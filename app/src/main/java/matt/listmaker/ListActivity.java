aefc875390631e3f87211cb06a3f4f4a72b594da
Matthew <matthewhunter404@gmail.com>
2017-02-06 23:01:33 +0300

    alters the ListAcivity to assign the List Adapter to its Listview

--- app/src/main/java/matt/listmaker/ListActivity.java
+++ app/src/main/java/matt/listmaker/ListActivity.java
@@ -4,17 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by MattsDesktop on 02/02/2017.
  */
 
 public class ListActivity extends AppCompatActivity {
     private ListView mListView;
+    private ListActivityListAdapter mItemAdapter;
+    private List<ListItem> mListItems = new ArrayList<ListItem>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         mListView = (ListView) findViewById(R.id.ListObjectDisplayList);
+        mItemAdapter = new ListActivityListAdapter(this, R.layout.item_list_row, R.id.item_list_textview, mListItems);
+        mListView.setAdapter(mItemAdapter);
     }
 
 
