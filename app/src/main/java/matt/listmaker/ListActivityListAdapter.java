7309bcaf4418a9f7ce8c28191060b2be407e046d
Matthew <matthewhunter404@gmail.com>
2017-02-22 22:31:21 +0300

    adds padding to the ListActivity view

--- app/src/main/java/matt/listmaker/ListActivityListAdapter.java
+++ app/src/main/java/matt/listmaker/ListActivityListAdapter.java
@@ -2,12 +2,15 @@ package matt.listmaker;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,6 +21,7 @@ public class ListActivityListAdapter extends ArrayAdapter<ListItem> {
     Context context;
     int layoutResourceId;
     List<ListItem> data = null;
+
 
     public ListActivityListAdapter(Context context, int layoutResourceId, int textlayoutResourceId,  List<ListItem> data) {
         super(context, layoutResourceId,textlayoutResourceId, data);
@@ -48,6 +52,9 @@ public class ListActivityListAdapter extends ArrayAdapter<ListItem> {
         holder.Item_Name_Textview.setText(displaytext);
         return row;
     }
+
+
+
     //The main adapter holder is simply a class to hold various views, so that they don't have to be constantly found every time the the program needs to
     //alther the data in them in some way. These variables are grouped together in one class for convenience
     static class ListActivityAdapterHolder
