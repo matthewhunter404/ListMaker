57a830890f8e276d514f9b854126e830b7fb5e35
Matthew <matthewhunter404@gmail.com>
2017-03-05 09:40:35 +0300

    Selecting the settings option from the dropdown menu calls the Settings Activity.

--- app/src/main/java/matt/listmaker/MainActivity.java
+++ app/src/main/java/matt/listmaker/MainActivity.java
@@ -93,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         else if (id == R.id.delete_all) {
