4c1c391ad6eb704ee03369e2dedbdb8416857ba1
Matthew <matthewhunter404@gmail.com>
2017-03-05 09:02:54 +0300

    adds an OnCreate procedure to the SettingsActivity

--- app/src/main/java/matt/listmaker/SettingsActivity.java
+++ app/src/main/java/matt/listmaker/SettingsActivity.java
@@ -1,5 +1,6 @@
 package matt.listmaker;
 
+import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
@@ -10,6 +11,11 @@ import android.preference.PreferenceActivity;
 public class SettingsActivity extends PreferenceActivity
         implements Preference.OnPreferenceChangeListener {
 
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+        // updated when the preference changes.
+    }
 
 }
